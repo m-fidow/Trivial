3b1b0a7ed624a9e6c86c0717f8479e65e1bb5521
m-fidow <muna.a.fidow@gmail.com>
2021-05-25 17:49:39 +0100

    hide screen1 and reveal screen 2 when start button is clicked

---
+++ Screen1.js
@@ -0,0 +1,8 @@
+let startButton = document.querySelector(".start-button");
+let screen1 = document.querySelector("#screen1");
+let screen2 = document.querySelector("#screen2");
+function hideScreen() {
+  screen1.classList.add("hide");
+  screen2.classList.remove("hide");
+}
+startButton.addEventListener("click", hideScreen);
